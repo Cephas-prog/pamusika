69a81f8f0210bf6faa89edb4ddeeb8f9c9d83336
Cephas-prog <cephas-gondwe@hotmail.co.uk>
2022-05-01 15:41:10 +0100

    push to git

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">pamsika</a>
+      </header>
+      <main>
+        products lists
+      </main>
+    </div>
+  );
+}
+
+export default App;
